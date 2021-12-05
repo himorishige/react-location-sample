6279238292ad224b00e10bb2d24caaa42b1ccc25
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-12-05 21:12:49 +0900

    Router.tsxのLocationGenerics部分修正

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { Route, ReactLocation, MakeGenerics } from 'react-location';
 import { ReactLocationSimpleCache } from 'react-location-simple-cache';
 import { Home } from './components/Home';
-import { PostIndex } from './components/PostIndex';
 import type { Post } from './types';
 
 export type LocationGenerics = MakeGenerics<{
   LoaderData: {
     posts: Post[];
     post: Post;
+  };
+  Params: {
+    postId?: string;
   };
 }>;
 
